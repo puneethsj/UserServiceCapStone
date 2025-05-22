f058d71299fa55048c9914fe9779a6d8013f3af8
Puneeth SJ <puneeth.sj@hotmail.com>
2025-05-22 11:14:18 +0530

    Initial commit of UserService

---
+++ src/main/java/com/example/userservicecapstone/UserServiceCapStoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.userservicecapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceCapStoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceCapStoneApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/userservicecapstone/models/Role.java
@@ -0,0 +1,13 @@
+package com.example.userservicecapstone.models;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+public class Role extends Base {
+    private String value;
+}
